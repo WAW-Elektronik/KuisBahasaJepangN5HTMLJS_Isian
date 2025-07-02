590f7e68b4c36da1a27b4bab34e680f577baa01e
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-07-02 12:43:50 +0700

    Add files via upload

---
+++ Questions/KataKerja/Isian_PerubahanBentukPenggunaan/PerubahanBentukPenggunaan.js
@@ -0,0 +1,70 @@
+window.soalKuis = [
+    // ~ます
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~ませんか" ,jawaban:"Ajakan Lebih Sopan"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~ましょう" ,jawaban:"Ajakan Sopan"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~たい" ,jawaban:"Keinginan melakukan sesuatu"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~ましょうか" ,jawaban:"Menawarkan suatu perbuatan orang lain"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~に いきます" ,jawaban:"Pergi untuk melakukan suatu hal"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~ながら" ,jawaban:"Melakukan dua aktivitas secara bersamaan (sambil)"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~かた" ,jawaban:"terkait cara melakukan sesuatu"},
+    // ~て
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て ください" ,jawaban:"Perintah Sopan"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て います" ,jawaban:"Menyatakan aktifitas yang sedang berlangsung saat ini atau menyatakan aktifitas yang dulu hingga sekarang masih berlangsung"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て も" ,jawaban:"walaupun/meskipun"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て もいいです" ,jawaban:"Perizinan (boleh)"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て はいけません" ,jawaban:"Menyatakan ketidakbolehan melakukan suatu hal"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て から" ,jawaban:"Setelah melakukan aktifitas kemudian melakukan aktifitas berikutnya"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て, ~て, ~て" ,jawaban:"Menyatakan kronologi/urutan aktivitas dalam suatu kalimat"},
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て あげます" ,jawaban:"Memberikan atau melakukan sebuah perbuatan dari saya orang lain"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て もらいます" ,jawaban:"Menerima sebuah perbuatan dari orang lain saya"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~て くれます" ,jawaban:"Orang lain melakukan perbuatan untuk saya"},
+    // ~な い 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~な い で ください" ,jawaban:"perintah larangan Sopan"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~な ければなりません" ,jawaban:"Keharusan melakukan sesuatu"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~な くてもいいです" ,jawaban:"Diperbolehkan tidak melakukan suatu hal"},
+    // ~る
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~る ことが できます" ,jawaban:"Menyatakan kemampuan/bisa"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~る こと です" ,jawaban:"Merubah status kata kerja kata benda"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~る まえ に ~" ,jawaban:"Sebelum melakukan aktivitas B, melakukan aktivitas A"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~る と" ,jawaban:"Mengemukakan kalimat persyaratan dengan akibat yang sudah pasti"},
+    // ~た 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~た ことが あります" ,jawaban:"Pengalaman (pernah)  melakukan suatu hal"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~た, ~た り, ~た り します" ,jawaban:"Mengungkapkan 2 atau 3 dari sekian banyak aktivitas tanpa memperhatikan kronologi/urutan kejadian"}, 
+    {pertanyaan: "Apa maksud/makna dari perubahan bentuk kata kerja ~た ら" ,jawaban:"Menyatakan kalimat perandaian/persyaratan (seandainya, jika, kalau)"},
+    // ~ます
+    {pertanyaan: "Ajakan lebih sopan merupakan perubahan bentuk kata kerja apa", jawaban:"~ませんか"}, 
+    {pertanyaan: "Ajakan sopan merupakan perubahan bentuk kata kerja apa", jawaban:"~ましょう"},
+    {pertanyaan: "Keinginan melakukan sesuatu merupakan perubahan bentuk kata kerja apa", jawaban:"~たい"},
+    {pertanyaan: "Menawarkan suatu perbuatan kepada orang lain merupakan perubahan bentuk kata kerja apa", jawaban:"~ましょうか"},
+    {pertanyaan: "Pergi untuk melakukan suatu hal merupakan perubahan bentuk kata kerja apa", jawaban:"~に いきます"},
+    {pertanyaan: "Melakukan dua aktivitas secara bersamaan (sambil) merupakan perubahan bentuk kata kerja apa", jawaban:"~ながら"},
+    {pertanyaan: "Terkait cara melakukan sesuatu merupakan perubahan bentuk kata kerja apa", jawaban:"~かた"},
+    
+    // ~て
+    {pertanyaan: "Perintah sopan merupakan perubahan bentuk kata kerja apa", jawaban:"~て ください"},
+    {pertanyaan: "Menyatakan aktivitas yang sedang berlangsung atau berlangsung sejak dulu hingga sekarang merupakan perubahan bentuk kata kerja apa", jawaban:"~て います"},
+    {pertanyaan: "Walaupun/meskipun merupakan perubahan bentuk kata kerja apa", jawaban:"~て も"},
+    {pertanyaan: "Perizinan (boleh) merupakan perubahan bentuk kata kerja apa", jawaban:"~て もいいです"},
+    {pertanyaan: "Ketidakbolehan melakukan suatu hal merupakan perubahan bentuk kata kerja apa", jawaban:"~て はいけません"},
+    {pertanyaan: "Setelah melakukan aktivitas kemudian melakukan aktivitas berikutnya merupakan perubahan bentuk kata kerja apa", jawaban:"~て から"},
+    {pertanyaan: "Menyatakan kronologi/urutan aktivitas dalam suatu kalimat merupakan perubahan bentuk kata kerja apa", jawaban:"~て, ~て, ~て"},
+    {pertanyaan: "Memberikan atau melakukan sebuah perbuatan dari saya kepada orang lain merupakan perubahan bentuk kata kerja apa", jawaban:"~て あげます"},
+    {pertanyaan: "Menerima sebuah perbuatan dari orang lain kepada saya merupakan perubahan bentuk kata kerja apa", jawaban:"~て もらいます"},
+    {pertanyaan: "Orang lain melakukan perbuatan untuk saya merupakan perubahan bentuk kata kerja apa", jawaban:"~て くれます"},
+    
+    // ~ない
+    {pertanyaan: "Perintah larangan sopan merupakan perubahan bentuk kata kerja apa", jawaban:"~ないで ください"},
+    {pertanyaan: "Keharusan melakukan sesuatu merupakan perubahan bentuk kata kerja apa", jawaban:"~なければなりません"},
+    {pertanyaan: "Diperbolehkan tidak melakukan suatu hal merupakan perubahan bentuk kata kerja apa", jawaban:"~なくてもいいです"},
+    
+    // ~る
+    {pertanyaan: "Menyatakan kemampuan atau bisa merupakan perubahan bentuk kata kerja apa", jawaban:"~る ことが できます"},
+    {pertanyaan: "Merubah status kata kerja menjadi kata benda merupakan perubahan bentuk kata kerja apa", jawaban:"~る こと"},
+    {pertanyaan: "Sebelum melakukan aktivitas B, melakukan aktivitas A merupakan perubahan bentuk kata kerja apa", jawaban:"~る まえ に"},
+    {pertanyaan: "Mengemukakan kalimat persyaratan dengan akibat yang sudah pasti merupakan perubahan bentuk kata kerja apa", jawaban:"~る と"},
+    
+    // ~た
+    {pertanyaan: "Pengalaman (pernah melakukan suatu hal) merupakan perubahan bentuk kata kerja apa", jawaban:"~た ことが あります"},
+    {pertanyaan: "Mengungkapkan 2 atau 3 dari sekian banyak aktivitas tanpa memperhatikan urutan merupakan perubahan bentuk kata kerja apa", jawaban:"~た り ~た り します"},
+    {pertanyaan: "Menyatakan kalimat perandaian/persyaratan (seandainya, jika, kalau) merupakan perubahan bentuk kata kerja apa", jawaban:"~た ら"},
+];
